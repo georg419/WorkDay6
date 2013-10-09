a8f22e598e1a8282089ebf0bf7dda91718ed8192
ef <ff@ge.com>
2013-10-09 15:26:38 -0500

    I solved it

--- PerfectSquares/PerfectSquares/Form1.vb
+++ PerfectSquares/PerfectSquares/Form1.vb
@@ -1,9 +1,16 @@
 ﻿Public Class Form1
 
     Private Sub btnDisplay_Click(sender As System.Object, e As System.EventArgs) Handles btnDisplay.Click
+        Dim n As Integer = 1
 
-        lstResult.Items.Clear()
-        lstResult.Items.Add("Not implemented yet")
+        Do While (n <= 10)
+
+
+            lstResult.Items.Add(n ^ 2)
+            n += 1
+        Loop
+        'lstResult.Items.Clear()
+        'lstResult.Items.Add("Not implemented yet")
 
     End Sub
 End Class
--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,16 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim pop As Double = 7000000000.0
+        Dim year As Integer = 2012
+        Do While pop > 6000000.0
+            pop = pop / 2
+            year = year - 50
 
-        MessageBox.Show("Not implemented yet", "Error")
+        Loop
+
+
+        MessageBox.Show(CStr(year))
 
     End Sub
 End Class
